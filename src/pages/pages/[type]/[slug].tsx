a95339687d0f9b3e3366f65f900779e5ffdbf19b
komnb14 <komnb14@gmail.com>
2023-02-16 01:46:50 +0900

    first

--- src/pages/pages/[type]/[slug].tsx
+++ src/pages/pages/[type]/[slug].tsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import {GetServerSideProps, NextPageContext} from "next";
+import {NextPageContext} from "next";
 import {URLPattern} from "next/server";
 
 
-export function parsePath(pathname: string, ctx: NextPageContext): any {
+function parsePath(pathname: string, ctx: NextPageContext): any {
     const pattern = new URLPattern({pathname});
     const result = pattern.exec(`http://host${ctx.req?.url}`);
     return result?.pathname.groups || {};
 }
 
 export const getServerSideProps = async (ctx: NextPageContext) => {
-    console.log(ctx.query);
     const {slug, type} = parsePath('/pages/:type/:slug', ctx)
     return {
         props: {
             slug: slug,
             type: type,
-            ctx: ctx.query,
         }
     }
 };
 
-const Slug = ({slug, type,ctx}: { slug: number, type: string,ctx: NextPageContext }) => {
+const Slug = ({slug, type, ctx}: { slug: number, type: string, ctx: NextPageContext }) => {
 
-    console.log(type,slug)
     return (
         <div>
             <div>현재 URL :{`/pages/${type}/${slug}`}</div>
